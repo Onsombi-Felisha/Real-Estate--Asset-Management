a583c485d74b2b37fe14c9a42c8143d65aea428d
Onsombi-Felisha <felishangina@gmail.com>
2024-05-30 00:19:05 -0700

    main.py

--- main.py
+++ main.py
@@ -1,50 +1,31 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 import pickle
 import numpy as np
-
-app = FastAPI
-@app.get("/")
-async def read_root():
-    message = "Welcome to Prediction of Occupancy Levels and optimising real estate asset management strategies based on key metrics.\n" \
-              "Make a POST request to /predict with the following data in the request body: 'Asset Manager', 'Prop ID', 'Property Name', 'Property Type', 'Average Occupancy%', 'GLA SqFT'.'Gross Value','Occupied SqFt','Vacant SqFt'.\n" \
-              "For API documentation, visit: http://localhost:8000/docs"
-
-    return {message}
+from pydantic import BaseModel
+from typing import List
 
 # Load the trained models
 with open('logisticregression_model.pkl', 'rb') as file:
     logistic_model = pickle.load(file)
 
-with open('DecisionTreeClassifier_pickle', 'rb') as file:
-    decision_tree_model = pickle.load(file)
-
-with open('RandomForestClassifier_pickle', 'rb') as file:
-    random_forest_model = pickle.load(file)
-
 app = FastAPI()
+
 
 # Define the data model for incoming requests
 class Item(BaseModel):
-    features: list
+    features: List[float]
 
-# Expose the prediction functionality, make a prediction from the passed data and
-# return the predicted
+# Function to translate prediction value to human-readable format
+def translate_prediction(prediction: int) -> str:
+    if prediction == 1:
+        return "hundred average occupancy ratio"
+    else:
+        return "unknown"  # Add more cases as needed
 
+# Expose the prediction functionality, make a prediction from the passed data and return the predicted
 @app.post("/predict/logistic")
 def predict_logistic(item: Item):
     features = np.array(item.features).reshape(1, -1)
     prediction = logistic_model.predict(features)
-    return {"prediction": int(prediction[0])}
-
-@app.post("/predict/decision_tree")
-def predict_decision_tree(item: Item):
-    features = np.array(item.features).reshape(1, -1)
-    prediction = decision_tree_model.predict(features)
-    return {"prediction": int(prediction[0])}
-
-@app.post("/predict/random_forest")
-def predict_random_forest(item: Item):
-    features = np.array(item.features).reshape(1, -1)
-    prediction = random_forest_model.predict(features)
-    return {"prediction": int(prediction[0])}
+    translated_prediction = translate_prediction(int(prediction[0]))
+    return {"prediction": translated_prediction}
