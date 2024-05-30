9c21d940b1754aa5c6772f46e23fc8c3be25c28d
Onsombi-Felisha <felishangina@gmail.com>
2024-05-29 23:37:23 -0700

    deleted:    DecisionTreeClassifier_pickle 	modified:   README.md      deleted:    RandomForestClassifier_pickle      modified:   main.py 	deleted:    predictions.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     message = "Welcome to Prediction of Occupancy Levels and optimising real estate asset management strategies based on key metrics\n" \
-              "Make a POST request to /predict with the following data in the request body: 'Occupant SqFt', 'Vacant SqFt', 'GLA SqFt', 'Property Type', 'Asset Manger',\n" \
+              "Make a POST request to /predict with the following data in the request body: 'Occupant SqFt', 'Vacant SqFt', 'GLA SqFt', 'Property Type', 'Asset Manger''Gross Value',\n" \
               "For API documentation, visit: http://localhost:8000/docs"
     
     return PlainTextResponse(content=message, status_code=200)
@@ -23,6 +23,12 @@ async def read_root():
 class Item(BaseModel):
     features: List[float]
 
+# Function to translate prediction value to human-readable format
+def translate_prediction(prediction: int) -> str:
+    if prediction == 1:
+        return "Has a a hundred percent average occupancy"
+    else:
+        return "Has zero average occupany" 
 # Expose the prediction functionality, make a prediction from the passed data and return the predicted
 @app.post("/predict/logistic")
 def predict_logistic(item: Item):
